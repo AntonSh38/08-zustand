fbbceab5bfcd6f3a36623c0e1e5b651610164082
Anton Shuvalov <anshuvalov83@gmail.com>
2025-10-22 15:39:03 +0300

    finish homework

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-import TanStackProvider from '@/components/TanStackProvider/TanStackProvaider';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
