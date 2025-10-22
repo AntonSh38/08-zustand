0be466daf4ac24445180498f060cf48d77aa7c80
Anton Shuvalov <anshuvalov83@gmail.com>
2025-10-22 16:17:54 +0300

    finish homework

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useQuery } from '@tanstack/react-query';
-import css from './NoteDetails.module.css';
+import css from '../../../components/NoteDetails/NoteDetails.module.css';
 import { fetchNoteById } from '@/lib/api';
 import { useParams } from 'next/navigation';
 import { Note } from '@/types/note';
