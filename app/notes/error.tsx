bac168d78cdc7406ae1b8e81b18a18f3db285c84
Anton Shuvalov <anshuvalov83@gmail.com>
2025-10-22 16:25:06 +0300

    finish homework

---
+++ app/notes/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+interface ErrorMessageProps {
+  message?: string;
+}
+
+export default function ErrorMessage({ message }: ErrorMessageProps) {
+  return (
+    <p>Could not fetch note details. {message && <span>{message}</span>}</p>
+  );
+}
